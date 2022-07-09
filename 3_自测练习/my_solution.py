5a6d7dce9b2ad91364b2da71edbae0eb2e8b0a6f
sn-911 <512428493@qq.com>
2022-07-09 15:01:54 +0800

    push solution my_solution and test_case requirments

--- 3_自测练习/my_solution.py
+++ 3_自测练习/my_solution.py
@@ -1,9 +1,148 @@
 #!/usr/bin/env python3
+#coding=utf-8
 
+# Apriori算法用于解决频繁项集挖掘问题，并返回关联规则
+
+import pandas as pd
+
+# 读取数据
+def load_data_set():
+    data = []
+    DataSet = pd.read_excel('data_set.xlsx')  # 读取三元组
+    i = 0
+
+    while i < len(DataSet):
+        line = DataSet['ITEM'][i]
+        d = line.strip().split(',')
+        data.append(d)
+        i += 1
+
+    return data
+
+def create_C1(data_set):
+    C1 = set()  # 创建空集
+    for t in data_set:
+        for item in t:
+            item_set = frozenset([item])    # frozenset()函数将列表转换为集合
+            C1.add(item_set)    #
+    return C1
+
+def is_apriori(Ck_item, Lksub1):
+    """
+    判断是否为频繁项集
+    :param Ck_item: 候选项集
+    :param Lksub1: Lk-1频繁项集
+    :return: 是否为频繁项集
+    """
+    for item in Ck_item:
+        sub_Ck = Ck_item - frozenset([item])
+        if sub_Ck not in Lksub1:
+            return False
+    return True
+
+def create_Ck(Lksub1, k):
+    """
+    创建第k个候选项集的集合
+    :param Lksub1: # Lk-1频繁项集
+    :param k: 项集中最大的项的长度
+    :return: Ck 频繁项集
+    """
+    Ck = set()
+    len_Lksub1 = len(Lksub1) # Lk-1的长度
+    list_Lksub1 = list(Lksub1)
+    for i in range(len_Lksub1):
+        for j in range(1, len_Lksub1):
+            l1 = list(list_Lksub1[i])
+            l2 = list(list_Lksub1[j])
+            l1.sort()
+            l2.sort()
+
+            raise NotImplementedError('编写合并项集的代码')
+
+    return Ck
+
+
+def generate_Lk_by_Ck(data_set, Ck, min_support, support_data):
+    """
+    :param data_set: 数据集
+    :param Ck: 项集
+    :param min_support: 最小支持度
+    :param support_data: 候选项目的支持度
+    :return: Lk 候选项集
+    """
+    Lk = set() # 记录候选项集
+    item_count = {} # 记录项集的频率
+
+    for t in data_set: # 计算项集的频率
+        for item in Ck:
+            if item.issubset(t):
+                if item not in item_count:
+                    item_count[item] = 1
+                else:
+                    item_count[item] += 1
+
+    t_num = float(len(data_set))
+
+    for item in item_count:
+        if (item_count[item] / t_num) >= min_support:   # 记录所有满足条件的项集
+            Lk.add(item)
+            support_data[item] = item_count[item] / t_num
+    return Lk
+
+
+def generate_L(data_set, k, min_support):
+    """
+    产生频繁项集
+    :param data_set: 数据集
+    :param k: 项集中最大的项的长度
+    :param min_support: 最小支持度
+    :return: L,support_data  L为频繁项集，support_data为频繁项集的支持度
+    """
+    C1 = create_C1(data_set)
+    support_data = {}
+    L1 = generate_Lk_by_Ck(data_set, C1, min_support, support_data) # 第一个频繁项集
+    Lksub1 = L1.copy()
+    L = [] # 频繁项集列表
+    L.append(Lksub1)
+
+    for i in range(2, k + 1):
+        Ci = create_Ck(Lksub1, i) # 第i个频繁项集
+        Li = generate_Lk_by_Ck(data_set, Ci, min_support, support_data) # 第i个频繁项集
+        Lksub1 = Li.copy()
+        L.append(Lksub1)
+    return L, support_data
+
+
+def generate_rules(L, support_data, min_conf):
+    """
+    :param L: 频繁项集
+    :param support_data: 频繁项集的支持度
+    :param min_conf: 最小置信度
+    :return: rules_list 包含频繁项集的规则
+    """
+    big_rule_list = [] # 存储所有的关联规则
+    sub_set_list = [] # 存储所有的频繁项集
+    for i in range(0, len(L)):
+        for freq_set in L[i]:
+            for sub_set in sub_set_list:
+                if sub_set.issubset(freq_set):  # 如果频繁项集中的某个项集已经在之前的频繁项集中出现，则跳过
+                    conf = support_data[freq_set] / \
+                        support_data[freq_set - sub_set]
+                    big_rule = (freq_set - sub_set, sub_set, conf)
+                    if conf >= min_conf and big_rule \
+                        not in big_rule_list:   # 如果置信度大于最小置信度，则添加到关联规则列表
+                        big_rule_list.append(big_rule)
+            sub_set_list.append(freq_set)
+
+    return big_rule_list
 # 待测试程序
-def solution(x):
-    return x + 2  # 为了展示测试结果，故意将x+1误写作x+2
+def solution():
+    data = load_data_set()  # 加载数据集
+    L, support_data = generate_L(data, k=3, min_support=0.2) # 产生频繁项集
+    big_rules_list = generate_rules(L, support_data, min_conf=0.7)  # 产生关联规则
+    return set(big_rules_list)
 
 
 if __name__ == '__main__':
+    solution()
     pass
--- 3_自测练习/test_case.py
+++ 3_自测练习/test_case.py
@@ -5,4 +5,14 @@ from my_solution import solution
 
 # 测试用例
 def test_solution():
-    assert solution(8) == 9  # 判断输出结果，预期increment(8)应该为9
+
+    # 正确答案
+    correct_solution_rule = set([(frozenset({'E'}), frozenset({'B'}), 1.0), (frozenset({'E'}), 
+    frozenset({'A'}), 1.0), (frozenset({'D'}), frozenset({'B'}), 1.0), (frozenset({'E', 'B'}), 
+    frozenset({'A'}), 1.0), (frozenset({'E', 'A'}), frozenset({'B'}), 1.0), (frozenset({'E'}), 
+    frozenset({'B', 'A'}), 1.0)])
+
+    # 程序求解结果
+    result_rule = solution()
+    assert result_rule == correct_solution_rule
+
